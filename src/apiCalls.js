05ab5d2250121bd4145dab48090915770cd6517a
Oscar Cortez <oacortez98@gmail.com>
2022-02-21 10:16:25 -0700

    Add a post and get request for players

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,28 @@
 
-
 export const getAllCourses = () => {
 
   return fetch('https://discify-api.herokuapp.com/api/v1/courses')
+    .then(response => response.json())
+}
+
+export const addNewPlayer = (name) => {
+
+  return fetch('https://discify-api.herokuapp.com/api/v1/players', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      player: {
+        name: name
+      }
+    })
+  })
+  .then(response => response.json())
+}
+
+export const getAllPlayers = () => {
+  
+  return fetch('https://discify-api.herokuapp.com/api/v1/players')
     .then(response => response.json())
 }

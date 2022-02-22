0757118115dbb9b84c805e7f914fd14743337ec9
Grace Gardner <graceirisgardner@gmail.com>
2022-02-21 19:13:54 -0700

    added more tests

--- cypress/integration/Manage_spec.js
+++ cypress/integration/Manage_spec.js
@@ -1,3 +1,19 @@
 describe('Manage league component', () => {
 
+  beforeEach(() => {
+    cy.intercept('Get', 'https://discify-api.herokuapp.com/api/v1/players',
+    { fixture: 'all-players.json'})
+
+    cy.intercept('POST','https://discify-api.herokuapp.com/api/v1/players', { fixture: 'all-players.json'})
+
+    cy.visit('http://localhost:3000')
+      .get('a').eq(1).click()
+  });
+
+  it('should be able to add new players to the league', () => {
+    cy.get('input').type('Hannah')
+      .get('.add-player-btn').click()
+  })
+
+
 })
--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -1,3 +1,14 @@
 describe('Search courses fuctionality', () => {
 
+  beforeEach(() => {
+    cy.intercept('Get', 'https://discify-api.herokuapp.com/api/v1/players',
+    { fixture: 'courses.json'})
+
+    cy.visit('http://localhost:3000')
+  });
+
+  it('Should display all courses if there is no input', () => {
+      cy.get('.search-btn').click()
+        .get('.course-card')
+  })
 })

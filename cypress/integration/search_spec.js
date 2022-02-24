d72108300306e4c747ce361a7884ddac62b1b31d
Grace Gardner <graceirisgardner@gmail.com>
2022-02-23 22:08:52 -0700

    fix search test

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -3,14 +3,14 @@ describe('Search courses fuctionality', () => {
   it('Should display all courses if there is no input', () => {
       cy.visit('http://localhost:3000')
 
-      cy.get('.search-btn').click().click()
+      cy.get('.search-btn').click()
   })
 
   it('Should filter and display specified courses', () => {
       cy.visit('http://localhost:3000')
 
       cy.get('.search-bar').type('west')
-        .get('.search-btn').click().click()
+        .get('.search-btn').click()
 
   // cy.get('.course-card')
     // .contains('west')

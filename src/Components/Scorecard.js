9ea26319513f302b8d4fc97c30fd163e9bfd1119
Oscar Cortez <oacortez98@gmail.com>
2022-02-24 17:48:14 -0700

    Refactor functionality

--- src/Components/Scorecard.js
+++ src/Components/Scorecard.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { ScorecardInfo } from '../Contexts/ScorecardInfoProvider'
+import { CourseInfo } from '../Contexts/CourseInfoProvider'
 import { Errors } from '../Contexts/ErrorsProvider'
 import Player from './Player'
 import { scoreAHole, endRound } from '../apiCalls'
@@ -7,11 +8,52 @@ import {useNavigate} from 'react-router-dom'
 
 const Scorecard = () => {
   const { scorecard, setScorecard } = useContext(ScorecardInfo)
-  const [ currentHole, setCurrentHole ] = useState({})
+  const [ currentHole, setCurrentHole ] = useState({par: 3, number: 1, distance: 0})
   const {errorMessage, setErrorMessage} = useContext(Errors)
   const [inProgress, setInProgress] = useState(true)
+  const [scores, setScores] = useState([])
+  const {courseInfo, setCourseInfo} = useContext(CourseInfo)
   const navigate = useNavigate()
+  
+  
+  useEffect(() => {
+    setCurrentHole({
+      number: 1,
+      par: courseInfo.currentCourse.layout.holes.find(hole => hole.hole_number === 1).par,
+      distance: courseInfo.currentCourse.layout.holes.find(hole => hole.hole_number === 1).distance
+    })
+  }, [])
+  
+  const displayPlayers = () => {
+    return scorecard.map(player => {
+      return (
+        <Player
+          name={player.name}
+          key={player.id}
+          id={player.id}
+          par={courseInfo.currentCourse.layout.holes.find(hole => {
+            return hole.hole_number === currentHole.number
+          }).par}
+          totalScore={findTotalScore(player.id)}
+          changeScore={changeScore}
+        />
+      )
+    })
+  }
 
+  const findTotalScore = (id) => {
+    let playerScores = scores.filter(score => score.player_id === id) 
+      return playerScores.length > 0 ? playerScores.reduce((acc, player) => {
+        acc += player.score
+        return acc
+      }, 0) : 0
+  }
+
+  const checkLastHole = () => {
+    return inProgress ?
+      <button className='next-hole-btn' onClick={postHoleScores}>NEXT HOLE</button> :
+      <button className='end-round-btn' onClick={postHoleScores}>END ROUND</button>
+  }
 
   const currentPlayers = () => {
     const result = {}
@@ -27,99 +69,54 @@ const Scorecard = () => {
   }
 
   const changeScore = (id, score) => {
-    currentHole.players[id].score = score
+    const player = scorecard.find(player => player.id === id)
+    player.score = score
+    setScorecard([...scorecard, player])
     setCurrentHole(currentHole)
   }
 
-  useEffect(() => {
-    setCurrentHole({
-      number: 1,
-      players: currentPlayers(),
-      par: scorecard.layout.holes.find(hole => hole.hole_number === 1).par,
-      distance: scorecard.layout.holes.find(hole => hole.hole_number === 1).distance
-    })
-  }, [])
-
-  const displayPlayers = () => {
-    return scorecard.players.map(player => {
-      return (
-        <Player
-          name={player.name}
-          key={player.id}
-          id={player.id}
-          par={scorecard.layout.holes.find(hole => {
-            return hole.hole_number === currentHole.number
-          }).par}
-          totalScore={currentHole.players[`${player.id}`].totalScore}
-          changeScore={changeScore}
-        />
-      )
-    })
-  }
-
-  const nextHole = (prevHoleScores) => {
-    const hole = scorecard.layout.holes.find(hole => hole.hole_number === currentHole.number + 1)
-    if(hole.hole_number === scorecard.layout.holes.length){
+  const nextHole = () => {
+    const hole = courseInfo.currentCourse.layout.holes.find(hole => hole.hole_number === currentHole.number + 1)
+    if(hole.hole_number === courseInfo.currentCourse.layout.holes.length){
       setInProgress(false)
     }
     setCurrentHole({
       number: hole.hole_number,
-      players: updatePlayerScores(prevHoleScores),
       par: hole.par,
       distance: hole.distance
     })
   }
 
-  const updatePlayerScores = (prevHoleScores) => {
-    const updatedPlayers = {}
-    prevHoleScores.forEach(player => {
-      updatedPlayers[player.player_id] = {
-        name: currentHole.players[player.player_id].name,
-        id: currentHole.players[player.player_id].id,
-        score: 0,
-        totalScore: player.score + currentHole.players[player.player_id].totalScore
-      }
-    })
-    return updatedPlayers
-  }
+  // const updatePlayerScores = (prevHoleScores) => {
+  //   const updatedPlayers = {}
+  //   prevHoleScores.forEach(player => {
+  //     updatedPlayers[player.player_id] = {
+  //       name: currentHole.players[player.player_id].name,
+  //       id: currentHole.players[player.player_id].id,
+  //       score: 0,
+  //       totalScore: player.score + currentHole.players[player.player_id].totalScore
+  //     }
+  //   })
+  //   return updatedPlayers
+  // }
 
   const postHoleScores = () => {
-    const currentPlayers = Object.keys(currentHole.players)
-    const playerScores = currentPlayers.map(player_id => {
-      return {
-        player_id: currentHole.players[player_id].id,
-        strokes: currentHole.players[player_id].score
-      }
-    })
-
     const body = {
       hole: {
         hole_number: currentHole.number,
-        player_scores: playerScores
+        player_scores: scorecard.map(player => {
+          return {
+            player_id: player.id,
+            strokes: player.strokes
+          }
+        })
       }
     }
-    scoreAHole(body, scorecard.roundId)
-    .then(data => nextHole(data.scores))
-    .catch(error => setErrorMessage(error))
-  }
-
-  const postFinalScores = () => {
-    const currentPlayers = Object.keys(currentHole.players)
-    const playerScores = currentPlayers.map(player_id => {
-      return {
-        player_id: currentHole.players[player_id].id,
-        strokes: currentHole.players[player_id].score
-      }
+    scoreAHole(body, courseInfo.currentCourse.roundId)
+    .then(data => {
+      setScores(data.scores)
+      inProgress ? nextHole() : endCurrentRound()
     })
-
-    const body = {
-      hole: {
-        hole_number: currentHole.number,
-        player_scores: playerScores
-      }
-    }
-    scoreAHole(body, scorecard.roundId)
-    .then(() =>  endCurrentRound())
     .catch(error => setErrorMessage(error))
   }
 
@@ -135,16 +132,10 @@ const Scorecard = () => {
     .catch(error => setErrorMessage(error))
   }
 
-  const checkLastHole = () => {
-    return inProgress ?
-      <button className='next-hole-btn' onClick={postHoleScores}>NEXT HOLE</button> :
-      <button className='end-round-btn' onClick={postFinalScores}>END ROUND</button>
-  }
-
   return (
     <div>
       <h2>Hole {currentHole.number}</h2>
-      { currentHole.players && displayPlayers() }
+      { scorecard.length && displayPlayers() }
       {checkLastHole()}
     </div>
   )
